d37752ecf6bbca85142527806077f0c5f0494a05
Pablo Cuba <pablofcc97@gmail.com>
2022-01-03 18:17:42 -0500

    actualizacion y agregado de elementos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let navMenu= document.querySelector('.nav__menu')
-let menuIcon= document.querySelector('.menu-logo')
-let closeIcon= document.querySelector('.close-logo')
+let menuIcon= document.querySelector('.nav__menu__menuLogo')
+let closeIcon= document.querySelector('.nav__menu__closeLogo')
 
 menuIcon.addEventListener('click',()=>{
     navMenu.classList.toggle('show')
